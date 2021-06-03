04963064ad276bab2e19c448aba5e82bb881cf2f
alexpintodiaz <alexanderp143@hotmail.com>
2021-06-02 21:57:12 -0500

    Funciones_Call_Aplly_Bing_21_04_2021

---
+++ main.js
@@ -0,0 +1,78 @@
+// declaración y expresiva. ECM
+
+let prim = (callback) => callback()/2;
+let segu = b => third()-3;
+let third = c => 5 + 3;
+
+console.log(prim(segu));
+
+
+// let suma = (...arg) => {
+//     return arg;
+// }
+// console.log(suma(1,2));
+
+// function suma(a,b){
+//     return a + b;
+// }
+// let suma = function(a,b){
+//     return a + b;
+// }
+
+
+// callback
+// let f1 = function(callback){
+//     return callback();
+// }
+// let f2 = function(){
+//     console.log("Hola soy la funcion 1");
+//     return "Hola soy la funcion 2";
+// }
+// console.log(f1(f2));
+
+
+// Auto ejecutadas
+// (function (){
+//     console.log("Hola");
+// })();
+
+
+// expresión
+// const nombre = function (...p1){
+//     console.log("Hola Mundo");
+//     return `Funcion dato ${p1}`
+// }
+
+// console.log(nombre("Alex"));
+// console.log(nombre("Paula"));
+
+
+
+// declaración.
+// function nombre(p1){
+//     console.log(p1);
+//     return `Ya se ejecuto la funcion`;
+// }
+
+// let resultado = nombre("Alex");
+// console.log(resultado);
+
+
+// expresión
+// const nombre = function (...p1){
+//     console.log("Hola Mundo");
+//     return `Funcion dato ${p1}`
+// }
+
+// console.log(nombre("Alex"));
+// console.log(nombre("Paula"));
+
+
+// declaración.
+// function nombre(p1){
+//     console.log(p1);
+//     return `Ya se ejecuto la funcion`;
+// }
+
+// let resultado = nombre("Alex");
+// console.log(resultado);
